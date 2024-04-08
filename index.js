22a5ce63b5b64adfa30870b09078e403a7102483
Joaquín Sal Aragón <joaquinsalaragon18@gmail.com>
2024-04-08 19:07:30 -0300

    html-completo

--- index.js
+++ index.js
@@ -1,1 +1,107 @@
-function jorge()
+/* PARA HABLAR POR PANTALLA */
+/* 
+let nombre = prompt("Hola! Quién esta visitando mi página?");
+
+alert("Bienvenido " + nombre) */
+
+/* ASIGNACION DE VALORES CONCATENACION */
+/* 
+saludo = "¡Hola pedro!";
+pregunta = " ¿Como estas?"
+
+frase = saludo + pregunta;
+
+document.write(frase) */
+
+/* CADENA DE TEXTO CON NUMEROS */
+/* 
+numero1 = 53;
+numero2 = 8;
+
+frase = "" + numero1 + numero2
+
+document.write(frase) */
+
+
+
+/* CONCATENACION
+numero1 = "53";
+numero2 = 8;
+
+frase = + numero1.concat(numero2)
+
+document.write(frase) */
+
+/* ejemplo boludo:
+nombre = "joako hudson"
+frase = "soy " + nombre + " y estoy caminando"
+
+document.write(frase) */
+
+
+/* ESCAPE DE COMILLAS SIMPLES Y COMILLAS DOBLES */
+
+/* nombre = "soy joako hudson";
+
+frase = "mi nombre es 'joako' y soy crack"
+
+document.write(frase)
+
+nombre = "soy joako hudson";
+
+frase = ' mi nombre es "joako" y soy crack'
+
+document.write(frase) */
+
+/* OPERADOR DE COMPARACIONES */
+
+/* FALSE */
+/* let numero = 23;
+let numero2 = 13;
+
+document.write(numero == numero2) */
+
+/* TRUE */
+/* let numero = 23;
+let numero2 = 23;
+
+document.write(numero == numero2) */
+
+
+/* let numero = 23;
+let numero2 = 13;
+let texto = "texto 1"
+let texto2 = "texto 2"
+
+document.write(texto != texto2) */
+
+/*
+== IGUAL
+!= INIGUAL
+=== ESTRICTAMENTE IGUAL
+!== NO ES ESTRICTAMENTE IGUAL
+A > B MAYOR QUE
+A >= B MAYOR O IGUAL QUE
+A < B MENOR QUE
+A <= B MENOR O IGUAL QUE
+ */
+
+
+/* num1 = 12;
+num2 = 24;
+num3 = 25;
+num4 = 92;
+num5 = 91;
+
+document.write(op) */
+
+/* holaComoEstas
+estoSeEscribeDeEstaForma
+paraPoderHacer
+lasCosasBien
+porEjemplo:
+
+document.getElementById() */
+
+
+
